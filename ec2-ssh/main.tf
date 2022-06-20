7b3999139d1262d0d20a6ba9a0e6234898471d5e
Tomer Admon <tomer.a@qualisystems.com>
2022-06-20 18:22:31 +0300

    Update main.tf

--- ec2-ssh/main.tf
+++ ec2-ssh/main.tf
@@ -78,7 +78,7 @@ resource "random_password" "password" {
 }
 
 resource "aws_instance" "this" {
-  ami             = ami-024437e14746c4dcb
+  ami             = "ami-024437e14746c4dcb"
   instance_type   = var.instance_type
   subnet_id       = aws_subnet.this.id
   security_groups = [aws_security_group.allow_ssh.id]
